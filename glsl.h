b7f1ca034397f7388c169ec2da69c2bb7c51c175
carlos <xroguebr@gmail.com>
2024-05-25 11:55:31 -0300

    mat4 transform ops

--- glsl.h
+++ glsl.h
@@ -108,23 +108,85 @@ vec4 normalize(const vec4& a) {
     return quat(a.x / l, a.y / l, a.z / l, a.w / l);
 }
 
-float distance(const vec2& a, const vec2& b) {
-    float dx = a.x - b.x;
-    float dy = a.y - b.y;
+float length(const vec2& a) {
+    float dx = a.x;
+    float dy = a.y;
     return sqrtf(dx*dx + dy*dy);
 }
-float distance(const vec3& a, const vec3& b) {
-    float dx = a.x - b.x;
-    float dy = a.y - b.y;
-    float dz = a.z - b.z;
+float length(const vec3& a) {
+    float dx = a.x;
+    float dy = a.y;
+    float dz = a.z;
     return sqrtf(dx*dx + dy*dy + dz*dz);
 }
-float distance(const vec4& a, const vec4& b) {
-    float dx = a.x - b.x;
-    float dy = a.y - b.y;
-    float dz = a.z - b.z;
-    float dw = a.w - b.w;
+float length(const vec4& a) {
+    float dx = a.x;
+    float dy = a.y;
+    float dz = a.z;
+    float dw = a.w;
     return sqrtf(dx*dx + dy*dy + dz*dz + dw*dw);
+}
+
+float distance(const vec2& a, const vec2& b) {
+    return length(a - b);
+}
+float distance(const vec3& a, const vec3& b) {
+    return length(a - b);
+}
+float distance(const vec4& a, const vec4& b) {
+    return length(a - b);
+}
+
+float determinant(const mat4& m) {
+    float n11 = m[0][0], n12 = m[1][0], n13 = m[2][0], n14 = m[3][0];
+    float n21 = m[0][1], n22 = m[1][1], n23 = m[2][1], n24 = m[3][1];
+    float n31 = m[0][2], n32 = m[1][2], n33 = m[2][2], n34 = m[3][2];
+    float n41 = m[0][3], n42 = m[1][3], n43 = m[2][3], n44 = m[3][3];
+
+    float t11 = n23 * n34 * n42 - n24 * n33 * n42 + n24 * n32 * n43 - n22 * n34 * n43 - n23 * n32 * n44 + n22 * n33 * n44;
+    float t12 = n14 * n33 * n42 - n13 * n34 * n42 - n14 * n32 * n43 + n12 * n34 * n43 + n13 * n32 * n44 - n12 * n33 * n44;
+    float t13 = n13 * n24 * n42 - n14 * n23 * n42 + n14 * n22 * n43 - n12 * n24 * n43 - n13 * n22 * n44 + n12 * n23 * n44;
+    float t14 = n14 * n23 * n32 - n13 * n24 * n32 - n14 * n22 * n33 + n12 * n24 * n33 + n13 * n22 * n34 - n12 * n23 * n34;
+
+    return n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
+}
+mat4 inverse(const mat4& m) {
+    float n11 = m[0][0], n12 = m[1][0], n13 = m[2][0], n14 = m[3][0];
+    float n21 = m[0][1], n22 = m[1][1], n23 = m[2][1], n24 = m[3][1];
+    float n31 = m[0][2], n32 = m[1][2], n33 = m[2][2], n34 = m[3][2];
+    float n41 = m[0][3], n42 = m[1][3], n43 = m[2][3], n44 = m[3][3];
+
+    float t11 = n23 * n34 * n42 - n24 * n33 * n42 + n24 * n32 * n43 - n22 * n34 * n43 - n23 * n32 * n44 + n22 * n33 * n44;
+    float t12 = n14 * n33 * n42 - n13 * n34 * n42 - n14 * n32 * n43 + n12 * n34 * n43 + n13 * n32 * n44 - n12 * n33 * n44;
+    float t13 = n13 * n24 * n42 - n14 * n23 * n42 + n14 * n22 * n43 - n12 * n24 * n43 - n13 * n22 * n44 + n12 * n23 * n44;
+    float t14 = n14 * n23 * n32 - n13 * n24 * n32 - n14 * n22 * n33 + n12 * n24 * n33 + n13 * n22 * n34 - n12 * n23 * n34;
+
+    float det = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
+    float idet = 1.0f / det;
+
+    mat4 ret;
+
+    ret[0][0] = t11 * idet;
+    ret[0][1] = (n24 * n33 * n41 - n23 * n34 * n41 - n24 * n31 * n43 + n21 * n34 * n43 + n23 * n31 * n44 - n21 * n33 * n44) * idet;
+    ret[0][2] = (n22 * n34 * n41 - n24 * n32 * n41 + n24 * n31 * n42 - n21 * n34 * n42 - n22 * n31 * n44 + n21 * n32 * n44) * idet;
+    ret[0][3] = (n23 * n32 * n41 - n22 * n33 * n41 - n23 * n31 * n42 + n21 * n33 * n42 + n22 * n31 * n43 - n21 * n32 * n43) * idet;
+
+    ret[1][0] = t12 * idet;
+    ret[1][1] = (n13 * n34 * n41 - n14 * n33 * n41 + n14 * n31 * n43 - n11 * n34 * n43 - n13 * n31 * n44 + n11 * n33 * n44) * idet;
+    ret[1][2] = (n14 * n32 * n41 - n12 * n34 * n41 - n14 * n31 * n42 + n11 * n34 * n42 + n12 * n31 * n44 - n11 * n32 * n44) * idet;
+    ret[1][3] = (n12 * n33 * n41 - n13 * n32 * n41 + n13 * n31 * n42 - n11 * n33 * n42 - n12 * n31 * n43 + n11 * n32 * n43) * idet;
+
+    ret[2][0] = t13 * idet;
+    ret[2][1] = (n14 * n23 * n41 - n13 * n24 * n41 - n14 * n21 * n43 + n11 * n24 * n43 + n13 * n21 * n44 - n11 * n23 * n44) * idet;
+    ret[2][2] = (n12 * n24 * n41 - n14 * n22 * n41 + n14 * n21 * n42 - n11 * n24 * n42 - n12 * n21 * n44 + n11 * n22 * n44) * idet;
+    ret[2][3] = (n13 * n22 * n41 - n12 * n23 * n41 - n13 * n21 * n42 + n11 * n23 * n42 + n12 * n21 * n43 - n11 * n22 * n43) * idet;
+
+    ret[3][0] = t14 * idet;
+    ret[3][1] = (n13 * n24 * n31 - n14 * n23 * n31 + n14 * n21 * n33 - n11 * n24 * n33 - n13 * n21 * n34 + n11 * n23 * n34) * idet;
+    ret[3][2] = (n14 * n22 * n31 - n12 * n24 * n31 - n14 * n21 * n32 + n11 * n24 * n32 + n12 * n21 * n34 - n11 * n22 * n34) * idet;
+    ret[3][3] = (n12 * n23 * n31 - n13 * n22 * n31 + n13 * n21 * n32 - n11 * n23 * n32 - n12 * n21 * n33 + n11 * n22 * n33) * idet;
+
+    return ret;
 }
 
 vec3 quat_rotate(const quat& q, const vec3& v) {
@@ -145,6 +207,11 @@ mat4 mat4_rotate(const vec3& a, float angle) {
         oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
         0.0,                                0.0,                                0.0,                                1.0
     };
+}
+void mat4_scale(mat4& m, const vec3& a) {
+    m.a[0] *= a.x;
+    m.a[1] *= a.y;
+    m.a[2] *= a.z;
 }
 void mat4_translate(mat4& m, const vec3& a) {
     m.a[12] += a.x;
@@ -216,6 +283,51 @@ mat4 quat_to_mat4(const quat& q) {
     m[3][3] = 1.0;
 
     return m;
+}
+
+mat4 mat4_compose(const vec3& position, const quat& rotation, const vec3& scale) {
+    mat4 m = quat_to_mat4(rotation);
+    mat4_scale(m, scale);
+    mat4_translate(m, position);
+    return m;
+}
+void mat4_decompose(mat4 m, vec3& position, quat& rotation, vec3& scale) {
+    float sx = length(vec3(m[0][0], m[0][1], m[0][2]));
+    float sy = length(vec3(m[1][0], m[1][1], m[1][2]));
+    float sz = length(vec3(m[2][0], m[2][1], m[2][2]));
+
+    // if determine is negative, we need to invert one scale
+    float det = determinant(m);
+    if (det < 0) {
+        sx = -sx;
+    }
+
+    position.x = m[3][0];
+    position.y = m[3][1];
+    position.z = m[3][2];
+
+    // scale the rotation part
+    float invSX = 1.0 / sx;
+    float invSY = 1.0 / sy;
+    float invSZ = 1.0 / sz;
+
+    m[0][0] *= invSX;
+    m[0][1] *= invSX;
+    m[0][2] *= invSX;
+
+    m[1][0] *= invSY;
+    m[1][1] *= invSY;
+    m[1][2] *= invSY;
+
+    m[2][0] *= invSZ;
+    m[2][1] *= invSZ;
+    m[2][2] *= invSZ;
+
+    rotation = mat4_to_quat(m);
+
+    scale.x = sx;
+    scale.y = sy;
+    scale.z = sz;
 }
 
 uint32_t part1by2(uint32_t n) {
