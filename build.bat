ffd7f1bc47697ee153f736e9fdcfb9614a0f13d2
carlos <xroguebr@gmail.com>
2025-10-09 12:34:35 -0300

    workgroup size as define

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL 2>&1 &&^
 cmake --build build -- --quiet &&^
+glslc shaders/matmul_coop.comp -std=460 --target-env=vulkan1.3 &&^
 build\evk_example.exe
