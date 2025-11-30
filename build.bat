d3b5742f2feec6b73dc9cc424feb0fa4fe22d586
carlos <xroguebr@gmail.com>
2025-11-30 08:39:38 -0300

    move llm test to other file

--- build.bat
+++ build.bat
@@ -1,13 +1,19 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL 2>&1 &&^
-cmake --build build -- --quiet &&^
-glslc shaders/matmul.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/matmul.comp.spv &&^
-glslc shaders/mse_loss.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/mse_loss.comp.spv &&^
-glslc shaders/sgd.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/sgd.comp.spv &&^
-glslc shaders/adam.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/adam.comp.spv &&^
-glslc shaders/add.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/add.comp.spv &&^
-glslc shaders/softmax.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/softmax.comp.spv &&^
-glslc shaders/cross_entropy.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/cross_entropy.comp.spv &&^
-glslc shaders/transpose.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/transpose.comp.spv &&^
-glslc shaders/flash_attention.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/flash_attention.comp.spv &&^
-glslc shaders/flash_attention_bwd.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/flash_attention_bwd.comp.spv &&^
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL 2>&1
+cmake --build build -- --quiet
+
+REM Detect if cmake build failed
+if errorlevel 1 (
+    echo Build failed!
+    exit /b 1
+)
+
+REM Compile each shader
+for %%s in (matmul mse_loss sgd adam add softmax cross_entropy transpose flash_attention flash_attention_bwd) do (
+    glslc shaders/%%s.comp -std=460 --target-env=vulkan1.3 -o shaders/bin/%%s.comp.spv
+    if errorlevel 1 (
+        exit /b 1
+    )
+)
+
 build\evk_example.exe
